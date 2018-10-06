46874e11794840714ff284e47d6dac9fe4efe701
Mateusz <matpiwowarski7@gmail.com>
2018-10-07 01:48:29 +0200

    centering the menu

--- MusicProcessingProgram/MusicProcessingProgram/menu.cpp
+++ MusicProcessingProgram/MusicProcessingProgram/menu.cpp
@@ -2,13 +2,12 @@
 #include "display_formatting.h"
 #include <iostream>
 
-#define VERTICAL_SPACING 4
-
 void Menu::show()
 {
-	multi_endl(VERTICAL_SPACING);
+	upper_space();
 	for (auto i = options.begin(); i != options.end(); ++i)
 	{
+		center_text();
 		std::cout << *i << std::endl;
 	}
 }
--- MusicProcessingProgram/MusicProcessingProgram/menu_vectors.h
+++ MusicProcessingProgram/MusicProcessingProgram/menu_vectors.h
@@ -6,6 +6,5 @@ std::vector<std::string> main_menu_strings
 	"> Create new sounds!",
 	"  Play music from a file",
 	"  Quit :("
-
 };
 #endif 
